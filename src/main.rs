4f97f4eb47b032145697e47fe1727f1e6eed183d
Korvin1337 <korvin1337@gmail.com>
2025-11-26 10:22:19 +0100

    added function to format the personnummer and make it ready for the luhn_check functions algorithm

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ fn main() {
         Regex::new(r"^(19|20)?\d{2}(0[1-9]|1[0-2])(0[1-9]|[12]\d|3[01])[-+]?[0-9]{4}$").unwrap();
 
     if re.is_match(&input) {
+        input = format_personnummer(&input).to_string();
         if luhn_check(&input) {
             println!("Korrekt personnummer!");
         }
@@ -22,7 +23,24 @@ fn main() {
 }
 
 fn luhn_check(personnummer: &str) -> bool {
+    /*let mut sum = 0;*/
     println!("Testing luhn on: {}", personnummer);
 
     return true;
 }
+
+fn format_personnummer(personnummer: &str) -> String {
+    let formatted_personnummer: String = personnummer.chars().filter(|c| c.is_digit(10)).collect();
+
+    let clean_format = if formatted_personnummer.len() > 10 {
+        &formatted_personnummer[2..]
+    } else {
+        &formatted_personnummer
+    };
+
+    println!("clean personnummer: {}", clean_format);
+
+    return clean_format.to_string();
+}
+
+/* 19970222-9222 */
